8cc7d6b067510db192806d2df9a81a23c5f11d9b
Brian Kim <brian.kim1080@gmail.com>
2023-09-02 17:58:37 -0500

    restore

--- preProcessing.py
+++ preProcessing.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
 
-url = 'file:///C:/Users/brian/Desktop/Derivation-Tree/NLP/articles/0907.2648.html'
+url = 'file:///C:/Users/brian/Desktop/Derivation-Tree/NLP/0907.2648.html'
 html = urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
 
@@ -17,31 +17,24 @@ for result in results:
 # Replace MathML with the text "mathequation"
 for script in soup(['math']):
     script.string = 'mathequation'
-
 # Get rid of annoying citations
 for script in soup(['cite']):
     script.extract()            # Removed
-
 # Adding paragraph break markers (parabreak) before each paragraph
 for script in soup(['p']):                      # For all the tags that have 'p'
     if script.get('class') == ['ltx_p']:        # If class tag is labelled with 'ltx_p'
         script.insert_before("parabreak")       # Insert marker before each paragraph
-
 # Adding edge markers (edge) before each equation
 for script in soup(['a']):                      # For all the tags that have 'a'
     if script.get('class') == ['ltx_ref']:      # If class tag is labelled with 'ltx_ref'
         script.insert_before("equationlink")       # Insert marker before each equation
-
 # get text
 text = soup.get_text(' ', strip=True)           #  strip whitespace from the beginning and end of each bit of text; No more '\n' in text
-
 # Debugging for printing entire text
 # print(text)
-
 # Output for string array; temp for holding strings 
 output = []
 temp = ''
-
 # Converting to string array format opposed to char
 for i in range(len(text)):
     temp += (text[i])                   # Adding chars together until find a space
@@ -49,15 +42,12 @@ for i in range(len(text)):
         output.append(temp[:-1])        # Add string to output array 
         temp = ''
         continue
-
 # Debugging for printing string array
 # print(output)
-
 # eqno array holds (equation #, line number) pair
 eqno = []
 idx = 1             # All equations start from 1
 asc = 97            # Ascii for 'a'
-
 # Checking for equations + line number
 for i in range(len(output)):
     if output[i] == 'References' or output[i] == 'references':      # Marks the end of paper
@@ -84,12 +74,9 @@ for i in range(len(output)):
         idx += 2
         asc = 98
         continue
-
 # Debugging for 
 # print(eqno)
-
 # Insert Paragraph Breaks into the (Eq #, idx #) output array 
-
 paraBreak = []                                  # New array with paragraph breaks
 counter = 0                                     # Counter for current Word in PDF
 temp = 0                                        # Placeholder for latest occurence of a paragraph break before equation
@@ -102,7 +89,6 @@ for i in range(len(eqno)):                       # Iterating through (Eq, idx nu
     paraBreak.append([(str(i+1)+'start'), temp])    # Append index to paragraph break list
     counter = eqno[i][1]                            # Set counter to start of next equation
     temp = eqno[i][1]                               # Set latest occurence of paragraph break to start of next equation
-
 # Extend range of text from end of equation to one sentence after
 exten = []                                      # List for holding the chunks of text after the equation
 for idx, eqNum in enumerate(eqno):
@@ -115,9 +101,6 @@ for idx, eqNum in enumerate(eqno):
             break
         startIdx += 1
     exten.append([str(eqno[idx][0])+'end',startIdx])      # Append current index as end of section
-
-
-
 # Debugging for paragraph breaks
 # print("Paragraph breaks: ", paraBreak)
 # print("No Paragraph breaks: ", eqno)
